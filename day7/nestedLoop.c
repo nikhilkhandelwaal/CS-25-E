1ffe032a913d0dd4ecc6f3d5d5ad966097fc2573
nikhilkhandelwaal <nikhilcoder061@gmail.com>
2025-09-17 11:24:32 +0530

    loops

--- day7/nestedLoop.c
+++ day7/nestedLoop.c
@@ -1,6 +1,26 @@
 #include<stdio.h>
 
-int main(){
-    
+int main() {
+
+    for (int j = 1; j <= 5; j++) {
+        for (int i = 1; i <= 5; i++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+
+
+
+
     return 0;
 }
+
+/*
+    *****
+    *****
+    *****
+    *****
+    *****
+
+*/
